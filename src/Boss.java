74a183db7bb49010b76c3347e9f972a96676efae
Администратор <@Administrator>
2016-03-16 22:07:40 +0200

    Колекцыи заполняются хорошо

--- src/Boss.java
+++ src/Boss.java
@@ -3,36 +3,49 @@ import java.util.ArrayList;
 public class Boss{
 	
         ArrayList<ArrayList> slots=new ArrayList<ArrayList>();
-        ArrayList<Integer> toys=new ArrayList<Integer>();
-        ArrayList aInt;
-        int Int;
+        ArrayList<Integer> toys=new ArrayList<Integer>();        
+       
        
     public static void main(String[] args){
     	
         Boss boss=new Boss();
         
-        boss.toys.add(new Integer(5));        
+        boss.toys.add(5);      
         //создание колекцый в слоте
         for(int i = 0; i<5; i++)
             boss.slots.add(new ArrayList<Integer>());        
         System.out.println("slots "+boss.slots);
-        //закидаем данные из колекцыи тойс
-        for(int i = 0; i<boss.slots.size(); i++){
-        	//boss.aInt=boss.slots.get(i);
-        	//boss.aInt=boss.toys;
-        	
-        	//boss.slots.get(i).removeAll(c)
-        	
-        	
+        //закидаем тойс
+        for(int i = 0; i<boss.slots.size(); i++){        	
+        	boss.slots.get(i).clear();
+        	boss.slots.get(i).addAll(boss.toys);        	
         }
         System.out.println("slots0 "+boss.slots);
         //изменяем тойс
-        boss.Int=boss.toys.get(0);
-        boss.Int=6;
+        boss.toys.clear();
+        for(int i = 0; i<5; i++) boss.toys.add(1);
+        
+        boss.slots.get(3).clear();
+    	boss.slots.get(3).addAll(boss.toys);
+        
         //тестим
-        boss.Int=(int) boss.slots.get(3).get(0);
-        System.out.println("slots1 "+boss.Int);
-        	
+        System.out.println("slots1 "+boss.slots);
+        System.out.println("Int "+boss.toys);
+        //изменяем тойс 0	
+        boss.toys.clear();
+        for(int i = 0; i<5; i++) boss.toys.add(2);
+        
+        System.out.println("slots1 "+boss.slots);
+        System.out.println("Int "+boss.toys);
+      //изменяем тойс 1	
+        boss.toys.clear();
+        for(int i = 0; i<7; i++) boss.toys.add(3);
+        
+        boss.slots.get(2).clear();
+    	boss.slots.get(2).addAll(boss.toys);
+        
+        System.out.println("slots1 "+boss.slots);
+        System.out.println("Int "+boss.toys);
         
     }
     
