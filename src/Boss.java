15ff4d69a22953b092691a7e3dd62ec514403bf9
vityaorjack <vityaorjack@mail.ru>
2016-03-17 12:40:28 +0200

    тест с класом Плеер

--- src/Boss.java
+++ src/Boss.java
@@ -3,52 +3,81 @@ import java.util.ArrayList;
 public class Boss{
 	
         ArrayList<ArrayList> slots=new ArrayList<ArrayList>();
-        ArrayList<Integer> toys=new ArrayList<Integer>();        
-       
+        ArrayList<Player> players=new ArrayList<Player>();        
+        static Player lvl;
        
     public static void main(String[] args){
     	
         Boss boss=new Boss();
         
-        boss.toys.add(5);      
+        boss.players.add(new Player());      
         //создание колекцый в слоте
         for(int i = 0; i<5; i++)
-            boss.slots.add(new ArrayList<Integer>());        
+            boss.slots.add(new ArrayList<Player>());        
         System.out.println("slots "+boss.slots);
-        //закидаем тойс
+        //закидаем плеерс
         for(int i = 0; i<boss.slots.size(); i++){        	
         	boss.slots.get(i).clear();
-        	boss.slots.get(i).addAll(boss.toys);        	
+        	boss.slots.get(i).addAll(boss.players);        	
         }
         System.out.println("slots0 "+boss.slots);
-        //изменяем тойс
-        boss.toys.clear();
-        for(int i = 0; i<5; i++) boss.toys.add(1);
+       //изменяем плеерс
+        boss.players.clear();
+        for(int i = 0; i<5; i++){
+        	boss.players.add(new Player());
+        	boss.players.get(i).lvl=(i+1);
+        }
         
         boss.slots.get(3).clear();
-    	boss.slots.get(3).addAll(boss.toys);
+    	boss.slots.get(3).addAll(boss.players);
         
         //тестим
         System.out.println("slots1 "+boss.slots);
-        System.out.println("Int "+boss.toys);
-        //изменяем тойс 0	
-        boss.toys.clear();
-        for(int i = 0; i<5; i++) boss.toys.add(2);
+        for(int i = 0; i<5; i++){
+        	lvl=(Player) boss.slots.get(3).get(i);
+        	System.out.print("slots3"+i+"="+lvl.lvl+" ");
+        }
+        System.out.println(" ");
+        //изменяем плеерс 1	
+        boss.players.clear();
+        boss.players.add(new Player());
+    	boss.players.get(0).lvl=5;
+    	
+    	boss.slots.get(2).clear();
+    	boss.slots.get(2).addAll(boss.players);
         
-        System.out.println("slots1 "+boss.slots);
-        System.out.println("Int "+boss.toys);
-      //изменяем тойс 1	
-        boss.toys.clear();
-        for(int i = 0; i<7; i++) boss.toys.add(3);
+    	System.out.println("slots1 "+boss.slots);
+        for(int i = 0; i<boss.slots.size(); i++){
+        	lvl=(Player) boss.slots.get(i).get(0);
+        	System.out.print("slots.get()"+i+"="+lvl.lvl+" ");
+        }
         
-        boss.slots.get(2).clear();
-    	boss.slots.get(2).addAll(boss.toys);
+      //изменяем плеерс 2	
+        boss.players.clear();
+        for(int i = 0; i<5; i++){
+        	boss.players.add(new Player());
+        	boss.players.get(i).lvl=6;
+        }
+    	
         
-        System.out.println("slots1 "+boss.slots);
-        System.out.println("Int "+boss.toys);
+        boss.slots.get(1).clear();
+    	boss.slots.get(1).addAll(boss.players);
+        
+    	System.out.println("slots2 "+boss.slots);
+    	System.out.println("супер проверка ");
+        for(int s = 0; s<boss.slots.size(); s++){
+        	System.out.println(" ");
+        	 for(int p = 0; p<boss.slots.get(s).size(); p++){
+        		 lvl=(Player) boss.slots.get(s).get(p);
+        		 System.out.print("slots"+s+"."+p+"="+lvl.lvl+" ");
+        	 }
+        }
         
     }
     
     
  
+}
+class Player{
+	int lvl;
 }
